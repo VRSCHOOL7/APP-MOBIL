f6a45ddd34e0f4c56e3050eadfee74866d4411f2
Mykhailo Yakyma <mexayakima1@gmail.com>
2022-03-23 16:13:05 +0100

    final

--- www/js/muestraDatos.js
+++ www/js/muestraDatos.js
@@ -1,4 +1,43 @@
 document.addEventListener('deviceready', onDeviceReady, false);
+$('.tabs').tabs({"swipeable": true});
+$('.sidenav').sidenav();
+$('select').formSelect();
+$('.scrollspy').scrollSpy();
+function loadCourse(IdCursos){
+    return function(){
+        $('#test-swipe-2').empty();
+        let text = "<div>"
+    
+        $.ajax({
+            method: "GET",
+            url: "https://vrschool7.herokuapp.com/api/get_course_details",
+            data: {session_token: localStorage.getItem('token_container'), courseID:IdCursos},
+            dataType: "json",      
+        }).done(function (dades) {
+            console.log(localStorage.getItem('token_container'));
+
+
+            $('#test-swipe-2').append("<h3>VR Tasks</h3>");
+            for (i in dades["course"]["vr_tasks"]){
+                let newElementList = "<li>"+ dades["course"]["vr_tasks"][i]["title"]+ "</li>";
+                text = text + newElementList
+                $('#test-swipe-2').append(text);
+            }
+            
+            $('#test-swipe-2').append("<h3>Tasks</h3>");
+            for (i in dades["course"]["tasks"]){
+                let newElementList = "<li>"+ dades["course"]["vr_tasks"][i]["title"]+ "</li>";
+                text = text + newElementList
+                $('#test-swipe-2').append(text);
+            }
+
+            
+            
+        }).fail(function () {
+            alert("ERROR");
+        });
+    };
+}
 
 function onDeviceReady() {
     $( document ).ready(function() {
@@ -13,10 +52,11 @@ function onDeviceReady() {
             for (i in dades["course_list"]){
                 console.log(dades["course_list"][i]["title"]);
                 let newelem = $("<a id='listelement' class='collection-item' href='#!'>"+dades["course_list"][i]["title"]+"</a>");
-                $("ul").append(newelem);
-            }
+                $("#lista_courses").append(newelem);
+                console.log(dades["course_list"][i]["_id"])
 
-        
+                newelem.click(loadCourse(dades["course_list"][i]["_id"]));
+            }
 
         }).fail(function () {
             alert("ERROR");
